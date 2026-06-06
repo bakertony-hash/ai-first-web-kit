4923b796b1f07f6278720596b7eb6c9c24ca9fb2
Tony Baker <tbaker@teslaforecast.com>
2026-06-06 19:04:03 +1200

    fix: address local verification issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["host.docker.internal"]
+  },
   test: {
     environment: "jsdom",
     globals: true,
